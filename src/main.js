f6f5e439001102a178a4758e2a7b8c2dde9d1bcb
Vlad Piersec <vlad.piersec@gmail.com>
2015-08-24 20:27:11 +0300

    Add cron file.

--- src/main.js
+++ src/main.js
@@ -1,35 +1,5 @@
 'use strict';
-// local dependencies
-var readAndParseRecipes = require('./recipesParser');
-var generateDirs = require('./generators/dirsGenerator');
-var generateTemplates = require('./generators/templatesGenerator');
-var buildMainPage = require('./builders/mainPageBuilder');
-var buildThemeIndex = require('./builders/themeIndexBuilder');
-var buildAllThemes = require('./builders/allThemesBuilder');
-var buildAllTags = require('./builders/allTagsBuilder');
-var buildTagsIndex = require('./builders/tagIndexBuilder');
-var buildCharts = require('./builders/chartsBuilder');
-// config vars
-var recipesDir = __dirname + '/../recipes/';
 
-function buildAllPages(allTemplates, allRecipes) {
-    buildMainPage(allTemplates);
-    buildAllThemes(allTemplates, allRecipes);
-    buildThemeIndex(allTemplates, allRecipes);
-    buildAllTags(allTemplates, allRecipes);
-    buildTagsIndex(allTemplates, allRecipes);
-    buildCharts(allTemplates);
-}
-
-function buildSite() {
-    readAndParseRecipes(recipesDir)
-        .then(function(allRecipes) {
-            generateDirs()
-                .then(generateTemplates)
-                .then(function(allTemplates) {
-                    buildAllPages(allTemplates, allRecipes);
-                });
-        }).done();
-}
-
+var buildSite = require('./buildSite');
+// build all
 buildSite();
