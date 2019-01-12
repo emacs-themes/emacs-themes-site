182039afe9b589a2e8f8fd60c89a2333701aecf6
caisah <vlad.piersec@protonmail.com>
2019-01-12 18:36:55 +0200

    Change node src for cron

--- cron.sh
+++ cron.sh
@@ -2,4 +2,4 @@
 
 # run cronjob
 # cron: 0,30 * * * * /home/caisah/emacs-themes-site/cron.sh
-node /home/caisah/emacs-themes-site/cron.js
+/home/caisah/.nvm/versions/node/v11.6.0/bin/node /home/caisah/emacs-themes-site/cron.js
