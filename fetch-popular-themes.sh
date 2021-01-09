051cf04783680e5c11c65c398cea3a04ae9c6517
caisah <vlad.piersec@protonmail.com>
2021-01-09 14:41:23 +0200

    Fix build scripts

--- fetch-popular-themes.sh
+++ fetch-popular-themes.sh
@@ -2,4 +2,4 @@
 
 # run cronjob
 # cron: 0 0 * * * /home/caisah/emacs-themes-site/fetch-popular-themes.sh
-/home/caisah/.nvm/versions/node/v11.6.0/bin/node /home/caisah/emacs-themes-site/fetch-popular-themes.js
+node ./pull-and-build.js && node ./fetch-popular-themes.js
